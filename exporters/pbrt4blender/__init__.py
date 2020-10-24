f38780c434b7c17f1898d621dc65340126375aa9
Pedro <thebountyrenderer@gmail.com>
2020-10-24 20:08:29 +0200

    Some Render Engine methods improvements an cleanup.

--- exporters/pbrt4blender/__init__.py
+++ exporters/pbrt4blender/__init__.py
@@ -1,6 +1,6 @@
 #-----------------------------------------------------------------------------#
 #
-#    This file is part of Pbrt4Blender, the Pbrt v4 integration for Blender
+#    This file is part of Pbrt4Blender, the Pbrt-v4 integration for Blender
 #
 #    Copyright 2020, Pedro A. "povmaniac"
 #
@@ -38,6 +38,16 @@ BIN_PATH = os.path.join(__path__[0], 'bin')
 sys.path.insert(0, BIN_PATH)
 os.environ['PATH'] = BIN_PATH + ';' + os.environ['PATH']
 
+#--------------------------
+# import modules
+#--------------------------
+if "bpy" in locals():
+    import importlib
+    if "ui" in locals:
+        importlib.reload(ui)
+    if "io" in locals:
+        importlib.reload(io)
+
 
 import sys
 import os 
@@ -48,22 +58,23 @@ from bpy_extras import view3d_utils
 import math
 import gpu
 
+from . import utils
+
 
 class Pbrt4RenderEngine(bpy.types.RenderEngine):
     # These three members are used by blender to set up the
     # RenderEngine; define its internal name, visible name and capabilities.
     bl_idname = "PBRT4"
-    bl_label = "BountyPbrt4"
+    bl_label = "Pbrt4Blender"
     bl_use_preview = True
 
 
-    # Init is called whenever a new render engine instance is created. Multiple
-    # instances may exist at the same time, for example for a viewport and final
-    # render.
+    # Init is called whenever a new render engine instance is created.
+    # Multiple instances may exist at the same time, for example for 
+    # a viewport and final render.
     def __init__(self):
-        self.scene_data = None
-        self.draw_data = None
-        self.tr = None
+        print('INIT ENGINE..')
+        
 
     # When the render engine instance is destroy, this is called. Clean up any
     # render engine data here, for example stopping running render threads.
@@ -75,7 +86,7 @@ class Pbrt4RenderEngine(bpy.types.RenderEngine):
         """Call pbrt binary with some hard-coded parameters"""
         os.chdir(folder)
         # call a bit modified killeroos scene
-        os.system('pbrt.exe ../kill/kill.pbrt')
+        os.system('pbrt ../kill/kill.pbrt')
 
     # This is the method called by Blender for both final renders (F12) and
     # small preview for materials, world and lights.
@@ -83,19 +94,20 @@ class Pbrt4RenderEngine(bpy.types.RenderEngine):
         scene = depsgraph.scene
         scale = scene.render.resolution_percentage / 100.0
         self.size_x = int(scene.render.resolution_x * scale)
-        self.size_y = int(scene.render.resolution_y * scale)        
+        self.size_y = int(scene.render.resolution_y * scale)
+        
 
         if not self.is_preview:
-            # try to load the render result into Blender
+            #
             result = self.begin_result(0, 0, 700, 700)
             #
             self.beautyRender(BIN_PATH)
             #
             lay = result.layers[0]
-            lay.load_from_file(BIN_PATH + "/killeroo-simple.exr")         
+            lay.load_from_file(BIN_PATH +"/killeroo-simple.exr")         
             #
-            self.end_result(result)
-
+            self.end_result(result) 
+        
         else:
             # Fill the render result with a flat color. The framebuffer is
             # defined as a list of pixels, each pixel itself being a list of
@@ -116,7 +128,7 @@ class Pbrt4RenderEngine(bpy.types.RenderEngine):
     # whenever the scene or 3D viewport changes. This method is where data
     # should be read from Blender in the same thread. Typically a render
     # thread will be started to do the work while keeping Blender responsive.
-    def view_update(self, context, depsgraph):
+    def _view_update(self, context, depsgraph):
         print('UPDATING..')
         
         
@@ -125,27 +137,10 @@ class Pbrt4RenderEngine(bpy.types.RenderEngine):
     # with OpenGL, and not perform other expensive work.
     # Blender will draw overlays for selection and editing on top of the
     # rendered image automatically.
-    def view_draw(self, context, depsgraph):
+    def _view_draw(self, context, depsgraph):
         print("DRAWING..")
 
 
-# RenderEngines also need to tell UI Panels that they are compatible with.
-# We recommend to enable all panels marked as BLENDER_RENDER, and then
-# exclude any panels that are replaced by custom panels registered by the
-# render engine, or that are not supported.
-def get_panels():
-    exclude_panels = {
-        'VIEWLAYER_PT_filter',
-        'VIEWLAYER_PT_layer_passes',
-    }
-
-    panels = []
-    for panel in bpy.types.Panel.__subclasses__():
-        if hasattr(panel, 'COMPAT_ENGINES') and 'BLENDER_RENDER' in panel.COMPAT_ENGINES:
-            if panel.__name__ not in exclude_panels:
-                panels.append(panel)
-
-    return panels
 
 def register():
     #
@@ -156,8 +151,6 @@ def register():
     # Register the RenderEngine
     bpy.utils.register_class(Pbrt4RenderEngine)
 
-    for panel in get_panels():
-        panel.COMPAT_ENGINES.add('PBRT4')
 
 def unregister():
     #
@@ -165,10 +158,6 @@ def unregister():
     io.unregister()
 
     bpy.utils.unregister_class(Pbrt4RenderEngine)
-
-    for panel in get_panels():
-        if 'PBRT4' in panel.COMPAT_ENGINES:
-            panel.COMPAT_ENGINES.remove('PBRT4')
 
 
 if __name__ == "__main__":
