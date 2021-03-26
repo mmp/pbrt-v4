mmp/pbrt-v4

commit f739c88750fe3837ce6b4ae11ced32adbb75bccc

Fix bug in PDF handling in BDPTIntegrator
Beyond being incorrect, this was causing fireflies in scenes using layered
BxDFs.

src/pbrt/cpu/integrators.cpp
@@ -2091,7 +2091,7 @@ int RandomWalk(const Integrator &integrator, SampledWavelengths &lambda,
         pstd::optional<BSDFSample> bs = bsdf.Sample_f(wo, u, sampler.Get2D(), mode);
         if (!bs)
             break;
-        pdfFwd = bs->pdf;
+        pdfFwd = bs->pdfIsProportional ? bsdf.PDF(wo, bs->wi, mode) : bs->pdf;
         anyNonSpecularBounces |= !bs->IsSpecular();
         beta *= bs->f * AbsDot(bs->wi, isect.shading.n) / bs->pdf;
         ray = isect.SpawnRay(ray, bsdf, bs->wi, bs->flags, bs->eta);

commit c8f8cb1d513ac89fce1fce493719db1b1671bfb6

Fix MLTIntegrator bug introduced in f739c88

src/pbrt/cpu/integrators.cpp
@@ -2579,7 +2579,7 @@ void MLTIntegrator::Render() {
         ErrorExit("No light carrying paths found during bootstrap sampling! "
                   "Are you trying to render a black image?");
     AliasTable bootstrapTable(bootstrapWeights);
-    Float b = (maxDepth + 1) / bootstrapWeights.size() *
+    Float b = Float(maxDepth + 1) / bootstrapWeights.size() *
               std::accumulate(bootstrapWeights.begin(), bootstrapWeights.end(), 0.);
 
     // Set up connection to display server, if enabled