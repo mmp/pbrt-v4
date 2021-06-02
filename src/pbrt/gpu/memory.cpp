7a441feab43607284960ea7ba6d5dc2225086b16
Matt Pharr <matt@pharr.org>
2021-06-02 13:54:34 -0700

    Add a few missing copyright notices

--- src/pbrt/gpu/memory.cpp
+++ src/pbrt/gpu/memory.cpp
@@ -1,3 +1,6 @@
+// pbrt is Copyright(c) 1998-2020 Matt Pharr, Wenzel Jakob, and Greg Humphreys.
+// The pbrt source code is licensed under the Apache License, Version 2.0.
+// SPDX: Apache-2.0
 
 #include <pbrt/gpu/memory.h>
 
