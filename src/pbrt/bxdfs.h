mmp/pbrt-v4

commit 1f926fd2bcab8ecf23d423cd046e0326a8e46094

DielectricBxDF: include BxDFFlags::Specular in flags when eta==1

src/pbrt/bxdfs.h
@@ -175,7 +175,7 @@ class DielectricBxDF {
     PBRT_CPU_GPU
     BxDFFlags Flags() const {
         if (eta == 1)
-            return BxDFFlags::Transmission;
+            return BxDFFlags::Transmission | BxDFFlags::Specular;
         else
             return BxDFFlags::Reflection | BxDFFlags::Transmission |
                    (mfDistrib.EffectivelySmooth() ? BxDFFlags::Specular

commit 4a57297b2e9e5c1d310cde5a000549f0421c8c15

Fix bug in DielectricBxDF::Flags() for eta==1.
We still need to consider the glossy case... (Bug introduced in
1f926fd2bcab8ecf23d423cd046e0326a8e46094.)

src/pbrt/bxdfs.h
@@ -175,7 +175,9 @@ class DielectricBxDF {
     PBRT_CPU_GPU
     BxDFFlags Flags() const {
         if (eta == 1)
-            return BxDFFlags::Transmission | BxDFFlags::Specular;
+            return BxDFFlags::Transmission |
+                   (mfDistrib.EffectivelySmooth() ? BxDFFlags::Specular
+                                                  : BxDFFlags::Glossy);
         else
             return BxDFFlags::Reflection | BxDFFlags::Transmission |
                    (mfDistrib.EffectivelySmooth() ? BxDFFlags::Specular