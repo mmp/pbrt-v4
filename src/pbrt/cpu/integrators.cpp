mmp/pbrt-v4

commit 20673d2d8ac6b90830c6a53d2ad694e73907233e

Update from 1st printing final book source
No functional changes; just some variable renamings and changes in comments
that correspond to fragment names that were edited.

src/pbrt/cpu/integrators.cpp
@@ -146,7 +146,7 @@ void ImageTileIntegrator::Render() {
         Film film = camera.GetFilm();
         DisplayDynamic(film.GetFilename(), Point2i(pixelBounds.Diagonal()),
                        {"R", "G", "B"},
-                       [&](Bounds2i b, pstd::span<pstd::span<float>> displayValue) {
+                       [&](Bounds2i b, pstd::span<pstd::span<Float>> displayValue) {
                            int index = 0;
                            for (Point2i p : b) {
                                RGB rgb = film.GetPixelRGB(pixelBounds.pMin + p,
@@ -521,7 +521,7 @@ void LightPathIntegrator::EvaluatePixelSample(Point2i pPixel, int sampleIndex,
     if (!sampledLight)
         return;
     Light light = sampledLight->light;
-    Float lightPDF = sampledLight->p;
+    Float p_l = sampledLight->p;
 
     // Sample point on light source for light path
     Float time = camera.SampleTime(sampler.Get1D());
@@ -544,7 +544,7 @@ void LightPathIntegrator::EvaluatePixelSample(Point2i pPixel, int sampleIndex,
                     // Compute visible light's path contribution and add to film
                     SampledSpectrum L = Le *
                                         DistanceSquared(cs->pRef.p(), cs->pLens.p()) *
-                                        cs->Wi / (lightPDF * pdf * cs->pdf);
+                                        cs->Wi / (p_l * pdf * cs->pdf);
                     camera.GetFilm().AddSplat(cs->pRaster, L, lambda);
                 }
             }
@@ -556,7 +556,7 @@ void LightPathIntegrator::EvaluatePixelSample(Point2i pPixel, int sampleIndex,
     // Initialize light path ray and weighted path throughput _beta_
     RayDifferential ray(les->ray);
     SampledSpectrum beta =
-        les->L * les->AbsCosTheta(ray.d) / (lightPDF * les->pdfPos * les->pdfDir);
+        les->L * les->AbsCosTheta(ray.d) / (p_l * les->pdfPos * les->pdfDir);
 
     while (true) {
         // Intersect light path ray with scene
@@ -634,7 +634,7 @@ SampledSpectrum PathIntegrator::Li(RayDifferential ray, SampledWavelengths &lamb
     SampledSpectrum L(0.f), beta(1.f);
     int depth = 0;
 
-    Float bsdfPDF, etaScale = 1;
+    Float p_b, etaScale = 1;
     bool specularBounce = false, anyNonSpecularBounces = false;
     LightSampleContext prevIntrCtx;
 
@@ -651,9 +651,9 @@ SampledSpectrum PathIntegrator::Li(RayDifferential ray, SampledWavelengths &lamb
                     L += beta * Le;
                 else {
                     // Compute MIS weight for infinite light
-                    Float lightPDF = lightSampler.PMF(prevIntrCtx, light) *
-                                     light.PDF_Li(prevIntrCtx, ray.d, true);
-                    Float w_b = PowerHeuristic(1, bsdfPDF, 1, lightPDF);
+                    Float p_l = lightSampler.PMF(prevIntrCtx, light) *
+                                light.PDF_Li(prevIntrCtx, ray.d, true);
+                    Float w_b = PowerHeuristic(1, p_b, 1, p_l);
 
                     L += beta * w_b * Le;
                 }
@@ -669,9 +669,9 @@ SampledSpectrum PathIntegrator::Li(RayDifferential ray, SampledWavelengths &lamb
             else {
                 // Compute MIS weight for area light
                 Light areaLight(si->intr.areaLight);
-                Float lightPDF = lightSampler.PMF(prevIntrCtx, areaLight) *
-                                 areaLight.PDF_Li(prevIntrCtx, ray.d, true);
-                Float w_l = PowerHeuristic(1, bsdfPDF, 1, lightPDF);
+                Float p_l = lightSampler.PMF(prevIntrCtx, areaLight) *
+                            areaLight.PDF_Li(prevIntrCtx, ray.d, true);
+                Float w_l = PowerHeuristic(1, p_b, 1, p_l);
 
                 L += beta * w_l * Le;
             }
@@ -739,7 +739,7 @@ SampledSpectrum PathIntegrator::Li(RayDifferential ray, SampledWavelengths &lamb
             break;
         // Update path state variables after surface scattering
         beta *= bs->f * AbsDot(bs->wi, isect.shading.n) / bs->pdf;
-        bsdfPDF = bs->pdfIsProportional ? bsdf.PDF(wo, bs->wi) : bs->pdf;
+        p_b = bs->pdfIsProportional ? bsdf.PDF(wo, bs->wi) : bs->pdf;
         DCHECK(!IsInf(beta.y(lambda)));
         specularBounce = bs->IsSpecular();
         anyNonSpecularBounces |= !bs->IsSpecular();
@@ -899,7 +899,7 @@ SampledSpectrum SimpleVolPathIntegrator::Li(RayDifferential ray,
                                 return false;
 
                             } else {
-                                // Handle null scattering event for medium sample
+                                // Handle null-scattering event for medium sample
                                 uMode = rng.Uniform<Float>();
                                 return true;
                             }
@@ -1024,18 +1024,18 @@ SampledSpectrum VolPathIntegrator::Li(RayDifferential ray, SampledWavelengths &l
                             return false;
                         }
 
-                        // Update _beta_ and _r_u_ for real scattering event
+                        // Update _beta_ and _r_u_ for real-scattering event
                         Float pdf = T_maj[0] * mp.sigma_s[0];
                         beta *= T_maj * mp.sigma_s / pdf;
                         r_u *= T_maj * mp.sigma_s / pdf;
 
                         if (beta && r_u) {
-                            // Sample direct lighting at volume scattering event
+                            // Sample direct lighting at volume-scattering event
                             MediumInteraction intr(p, -ray.d, ray.time, ray.medium,
                                                    mp.phase);
                             L += SampleLd(intr, nullptr, lambda, sampler, beta, r_u);
 
-                            // Sample new direction at real scattering event
+                            // Sample new direction at real-scattering event
                             Point2f u = sampler.Get2D();
                             pstd::optional<PhaseFunctionSample> ps =
                                 intr.phase.Sample_p(-ray.d, u);
@@ -1088,9 +1088,9 @@ SampledSpectrum VolPathIntegrator::Li(RayDifferential ray, SampledWavelengths &l
                         L += beta * Le / r_u.Average();
                     else {
                         // Add infinite light contribution using both PDFs with MIS
-                        Float lightPDF = lightSampler.PMF(prevIntrContext, light) *
-                                         light.PDF_Li(prevIntrContext, ray.d, true);
-                        r_l *= lightPDF;
+                        Float p_l = lightSampler.PMF(prevIntrContext, light) *
+                                    light.PDF_Li(prevIntrContext, ray.d, true);
+                        r_l *= p_l;
                         L += beta * Le / (r_u + r_l).Average();
                     }
                 }
@@ -1106,9 +1106,9 @@ SampledSpectrum VolPathIntegrator::Li(RayDifferential ray, SampledWavelengths &l
             else {
                 // Add surface light contribution using both PDFs with MIS
                 Light areaLight(isect.areaLight);
-                Float lightPDF = lightSampler.PMF(prevIntrContext, areaLight) *
-                                 areaLight.PDF_Li(prevIntrContext, ray.d, true);
-                r_l *= lightPDF;
+                Float p_l = lightSampler.PMF(prevIntrContext, areaLight) *
+                            areaLight.PDF_Li(prevIntrContext, ray.d, true);
+                r_l *= p_l;
                 L += beta * Le / (r_u + r_l).Average();
             }
         }
@@ -1304,7 +1304,7 @@ SampledSpectrum VolPathIntegrator::SampleLd(const Interaction &intr, const BSDF 
     pstd::optional<LightLiSample> ls = light.SampleLi(ctx, uLight, lambda, true);
     if (!ls || !ls->L || ls->pdf == 0)
         return SampledSpectrum(0.f);
-    Float lightPDF = sampledLight->p * ls->pdf;
+    Float p_l = sampledLight->p * ls->pdf;
 
     // Evaluate BSDF or phase function for light sample direction
     Float scatterPDF;
@@ -1383,7 +1383,7 @@ SampledSpectrum VolPathIntegrator::SampleLd(const Interaction &intr, const BSDF 
         lightRay = si->intr.SpawnRayTo(ls->pLight);
     }
     // Return path contribution function estimate for direct lighting
-    r_l *= r_p * lightPDF;
+    r_l *= r_p * p_l;
     r_u *= r_p * scatterPDF;
     if (IsDeltaLight(light.Type()))
         return beta * f_hat * T_ray * ls->L / r_l.Average();
@@ -1934,12 +1934,12 @@ int GenerateLightSubpath(const Integrator &integrator, SampledWavelengths &lambd
     RayDifferential ray(les->ray);
 
     // Generate first vertex of light subpath
-    Float lightPDF = lightSamplePDF * les->pdfPos;
-    path[0] = les->intr ? Vertex::CreateLight(light, *les->intr, les->L, lightPDF)
-                        : Vertex::CreateLight(light, ray, les->L, lightPDF);
+    Float p_l = lightSamplePDF * les->pdfPos;
+    path[0] = les->intr ? Vertex::CreateLight(light, *les->intr, les->L, p_l)
+                        : Vertex::CreateLight(light, ray, les->L, p_l);
 
     // Follow light subpath random walk
-    SampledSpectrum beta = les->L * les->AbsCosTheta(ray.d) / (lightPDF * les->pdfDir);
+    SampledSpectrum beta = les->L * les->AbsCosTheta(ray.d) / (p_l * les->pdfDir);
     PBRT_DBG("%s\n",
              StringPrintf(
                  "Starting light subpath. Ray: %s, Le %s, beta %s, pdfPos %f, pdfDir %f",
@@ -2363,7 +2363,7 @@ SampledSpectrum ConnectBDPT(const Integrator &integrator, SampledWavelengths &la
 
             if (sampledLight) {
                 Light light = sampledLight->light;
-                Float lightPDF = sampledLight->p;
+                Float p_l = sampledLight->p;
 
                 LightSampleContext ctx;
                 if (pt.IsOnSurface()) {
@@ -2383,7 +2383,7 @@ SampledSpectrum ConnectBDPT(const Integrator &integrator, SampledWavelengths &la
                 if (lightWeight && lightWeight->L && lightWeight->pdf > 0) {
                     EndpointInteraction ei(light, lightWeight->pLight);
                     sampled = Vertex::CreateLight(
-                        ei, lightWeight->L / (lightWeight->pdf * lightPDF), 0);
+                        ei, lightWeight->L / (lightWeight->pdf * p_l), 0);
                     sampled.pdfFwd = sampled.PDFLightOrigin(integrator.infiniteLights, pt,
                                                             lightSampler);
                     L = pt.beta * pt.f(sampled, TransportMode::Radiance) * sampled.beta;
@@ -2877,7 +2877,7 @@ void SPPMIntegrator::Render() {
 
                 // Follow camera ray path until a visible point is created
                 SPPMPixel &pixel = pixels[pPixel];
-                Float etaScale = 1, bsdfPDF;
+                Float etaScale = 1, p_b;
                 bool specularBounce = true, haveSetVisiblePoint = false;
                 LightSampleContext prevIntrCtx;
                 int depth = 0;
@@ -2894,9 +2894,9 @@ void SPPMIntegrator::Render() {
                                 L += beta * Le;
                             else {
                                 // Compute MIS weight for infinite light
-                                Float lightPDF = lightSampler.PMF(prevIntrCtx, light) *
-                                                 light.PDF_Li(prevIntrCtx, ray.d, true);
-                                Float w_b = PowerHeuristic(1, bsdfPDF, 1, lightPDF);
+                                Float p_l = lightSampler.PMF(prevIntrCtx, light) *
+                                            light.PDF_Li(prevIntrCtx, ray.d, true);
+                                Float w_b = PowerHeuristic(1, p_b, 1, p_l);
 
                                 L += beta * w_b * Le;
                             }
@@ -2928,9 +2928,9 @@ void SPPMIntegrator::Render() {
                         else {
                             // Compute MIS weight for area light
                             Light areaLight(si->intr.areaLight);
-                            Float lightPDF = lightSampler.PMF(prevIntrCtx, areaLight) *
-                                             areaLight.PDF_Li(prevIntrCtx, ray.d, true);
-                            Float w_l = PowerHeuristic(1, bsdfPDF, 1, lightPDF);
+                            Float p_l = lightSampler.PMF(prevIntrCtx, areaLight) *
+                                        areaLight.PDF_Li(prevIntrCtx, ray.d, true);
+                            Float w_l = PowerHeuristic(1, p_b, 1, p_l);
 
                             L += beta * w_l * Le;
                         }
@@ -2966,7 +2966,7 @@ void SPPMIntegrator::Render() {
                         etaScale *= Sqr(bs->eta);
 
                     beta *= bs->f * AbsDot(bs->wi, isect.shading.n) / bs->pdf;
-                    bsdfPDF = bs->pdfIsProportional ? bsdf.PDF(wo, bs->wi) : bs->pdf;
+                    p_b = bs->pdfIsProportional ? bsdf.PDF(wo, bs->wi) : bs->pdf;
 
                     SampledSpectrum rrBeta = beta * etaScale;
                     if (rrBeta.MaxComponentValue() < 1) {
@@ -3079,7 +3079,7 @@ void SPPMIntegrator::Render() {
                 if (!sampledLight)
                     continue;
                 Light light = sampledLight->light;
-                Float lightPDF = sampledLight->p;
+                Float p_l = sampledLight->p;
 
                 // Compute sample values for photon ray leaving light source
                 Point2f uLight0 = Sample2D();
@@ -3094,7 +3094,7 @@ void SPPMIntegrator::Render() {
                     continue;
                 RayDifferential photonRay = RayDifferential(les->ray);
                 SampledSpectrum beta = (les->AbsCosTheta(photonRay.d) * les->L) /
-                                       (lightPDF * les->pdfPos * les->pdfDir);
+                                       (p_l * les->pdfPos * les->pdfDir);
                 if (!beta)
                     continue;
 

src/pbrt/bxdfs.cpp
@@ -312,7 +312,7 @@ SampledSpectrum HairBxDF::f(Vector3f wo, Vector3f wi, TransportMode mode) const 
     Float cosTheta_i = SafeSqrt(1 - Sqr(sinTheta_i));
     Float phi_i = std::atan2(wi.z, wi.y);
 
-    // Compute $\cos \thetat$ for refracted ray
+    // Compute $\cos\,\thetat$ for refracted ray
     Float sinTheta_t = sinTheta_o / eta;
     Float cosTheta_t = SafeSqrt(1 - Sqr(sinTheta_t));
 
@@ -329,8 +329,9 @@ SampledSpectrum HairBxDF::f(Vector3f wo, Vector3f wi, TransportMode mode) const 
     Float phi = phi_i - phi_o;
     pstd::array<SampledSpectrum, pMax + 1> ap = Ap(cosTheta_o, eta, h, T);
     SampledSpectrum fsum(0.);
+
     for (int p = 0; p < pMax; ++p) {
-        // Compute $\sin \thetao$ and $\cos \thetao$ terms accounting for scales
+        // Compute $\sin\,\thetao$ and $\cos\,\thetao$ terms accounting for scales
         Float sinThetap_o, cosThetap_o;
         if (p == 0) {
             sinThetap_o = sinTheta_o * cos2kAlpha[1] - cosTheta_o * sin2kAlpha[1];
@@ -348,7 +349,7 @@ SampledSpectrum HairBxDF::f(Vector3f wo, Vector3f wi, TransportMode mode) const 
             cosThetap_o = cosTheta_o;
         }
 
-        // Handle out-of-range $\cos \thetao$ from scale adjustment
+        // Handle out-of-range $\cos\,\thetao$ from scale adjustment
         cosThetap_o = std::abs(cosThetap_o);
 
         fsum += Mp(cosTheta_i, cosThetap_o, sinTheta_i, sinThetap_o, v[p]) * ap[p] *
@@ -367,7 +368,7 @@ SampledSpectrum HairBxDF::f(Vector3f wo, Vector3f wi, TransportMode mode) const 
 pstd::array<Float, HairBxDF::pMax + 1> HairBxDF::ApPDF(Float cosTheta_o) const {
     // Initialize array of $A_p$ values for _cosTheta_o_
     Float sinTheta_o = SafeSqrt(1 - Sqr(cosTheta_o));
-    // Compute $\cos \thetat$ for refracted ray
+    // Compute $\cos\,\thetat$ for refracted ray
     Float sinTheta_t = sinTheta_o / eta;
     Float cosTheta_t = SafeSqrt(1 - Sqr(sinTheta_t));
 
@@ -406,7 +407,7 @@ pstd::optional<BSDFSample> HairBxDF::Sample_f(Vector3f wo, Float uc, Point2f u,
     pstd::array<Float, pMax + 1> apPDF = ApPDF(cosTheta_o);
     int p = SampleDiscrete(apPDF, uc, nullptr, &uc);
 
-    // Compute $\sin \thetao$ and $\cos \thetao$ terms accounting for scales
+    // Compute $\sin\,\thetao$ and $\cos\,\thetao$ terms accounting for scales
     Float sinThetap_o, cosThetap_o;
     if (p == 0) {
         sinThetap_o = sinTheta_o * cos2kAlpha[1] - cosTheta_o * sin2kAlpha[1];
@@ -424,7 +425,7 @@ pstd::optional<BSDFSample> HairBxDF::Sample_f(Vector3f wo, Float uc, Point2f u,
         cosThetap_o = cosTheta_o;
     }
 
-    // Handle out-of-range $\cos \thetao$ from scale adjustment
+    // Handle out-of-range $\cos\,\thetao$ from scale adjustment
     cosThetap_o = std::abs(cosThetap_o);
 
     // Sample $M_p$ to compute $\thetai$
@@ -455,7 +456,7 @@ pstd::optional<BSDFSample> HairBxDF::Sample_f(Vector3f wo, Float uc, Point2f u,
     // Compute PDF for sampled hair scattering direction _wi_
     Float pdf = 0;
     for (int p = 0; p < pMax; ++p) {
-        // Compute $\sin \thetao$ and $\cos \thetao$ terms accounting for scales
+        // Compute $\sin\,\thetao$ and $\cos\,\thetao$ terms accounting for scales
         Float sinThetap_o, cosThetap_o;
         if (p == 0) {
             sinThetap_o = sinTheta_o * cos2kAlpha[1] - cosTheta_o * sin2kAlpha[1];
@@ -473,10 +474,10 @@ pstd::optional<BSDFSample> HairBxDF::Sample_f(Vector3f wo, Float uc, Point2f u,
             cosThetap_o = cosTheta_o;
         }
 
-        // Handle out-of-range $\cos \thetao$ from scale adjustment
+        // Handle out-of-range $\cos\,\thetao$ from scale adjustment
         cosThetap_o = std::abs(cosThetap_o);
 
-        // Handle out-of-range $\cos \thetao$ from scale adjustment
+        // Handle out-of-range $\cos\,\thetao$ from scale adjustment
         cosThetap_o = std::abs(cosThetap_o);
 
         pdf += Mp(cosTheta_i, cosThetap_o, sinTheta_i, sinThetap_o, v[p]) * apPDF[p] *
@@ -515,7 +516,7 @@ Float HairBxDF::PDF(Vector3f wo, Vector3f wi, TransportMode mode,
     Float phi = phi_i - phi_o;
     Float pdf = 0;
     for (int p = 0; p < pMax; ++p) {
-        // Compute $\sin \thetao$ and $\cos \thetao$ terms accounting for scales
+        // Compute $\sin\,\thetao$ and $\cos\,\thetao$ terms accounting for scales
         Float sinThetap_o, cosThetap_o;
         if (p == 0) {
             sinThetap_o = sinTheta_o * cos2kAlpha[1] - cosTheta_o * sin2kAlpha[1];
@@ -533,7 +534,7 @@ Float HairBxDF::PDF(Vector3f wo, Vector3f wi, TransportMode mode,
             cosThetap_o = cosTheta_o;
         }
 
-        // Handle out-of-range $\cos \thetao$ from scale adjustment
+        // Handle out-of-range $\cos\,\thetao$ from scale adjustment
         cosThetap_o = std::abs(cosThetap_o);
 
         pdf += Mp(cosTheta_i, cosThetap_o, sinTheta_i, sinThetap_o, v[p]) * apPDF[p] *
@@ -1010,7 +1011,7 @@ SampledSpectrum MeasuredBxDF::f(Vector3f wo, Vector3f wi, TransportMode mode) co
         return SampledSpectrum(0);
     wm = Normalize(wm);
 
-    // Map $\wo$ and $\wm$ to the unit square $[0, 1]^2$
+    // Map $\wo$ and $\wm$ to the unit square $[0,\,1]^2$
     Float theta_o = SphericalTheta(wo), phi_o = std::atan2(wo.y, wo.x);
     Float theta_m = SphericalTheta(wm), phi_m = std::atan2(wm.y, wm.x);
     Point2f u_wo(theta2u(theta_o), phi2u(phi_o));

src/pbrt/bxdfs.h
@@ -477,7 +477,7 @@ class LayeredBxDF {
     SampledSpectrum f(Vector3f wo, Vector3f wi, TransportMode mode) const {
         SampledSpectrum f(0.);
         // Estimate _LayeredBxDF_ value _f_ using random sampling
-        // Set _wi_ and _wi_ for layered BSDF evaluation
+        // Set _wo_ and _wi_ for layered BSDF evaluation
         if (twoSided && wo.z < 0) {
             wo = -wo;
             wi = -wi;
@@ -539,7 +539,7 @@ class LayeredBxDF {
                 PBRT_DBG("beta: %f %f %f %f, w: %f %f %f, f: %f %f %f %f\n", beta[0],
                          beta[1], beta[2], beta[3], w.x, w.y, w.z, f[0], f[1], f[2],
                          f[3]);
-                // Possibly terminate layered BSDF random walk with Russian Roulette
+                // Possibly terminate layered BSDF random walk with Russian roulette
                 if (depth > 3 && beta.MaxComponentValue() < 0.25f) {
                     Float q = std::max<Float>(0, 1 - beta.MaxComponentValue());
                     if (r() < q)
@@ -778,7 +778,7 @@ class LayeredBxDF {
     Float PDF(Vector3f wo, Vector3f wi, TransportMode mode,
               BxDFReflTransFlags sampleFlags = BxDFReflTransFlags::All) const {
         CHECK(sampleFlags == BxDFReflTransFlags::All);  // for now
-        // Set _wi_ and _wi_ for layered BSDF evaluation
+        // Set _wo_ and _wi_ for layered BSDF evaluation
         if (twoSided && wo.z < 0) {
             wo = -wo;
             wi = -wi;

src/pbrt/cameras.h
@@ -272,6 +272,7 @@ class ProjectiveCamera : public CameraBase {
         cameraFromRaster = Inverse(screenFromCamera) * screenFromRaster;
     }
 
+  protected:
     // ProjectiveCamera Protected Members
     Transform screenFromCamera, cameraFromRaster;
     Transform rasterFromScreen, screenFromRaster;

src/pbrt/cpu/aggregates.cpp
@@ -290,7 +290,7 @@ BVHBuildNode *BVHAggregate::buildRecursive(ThreadLocal<Allocator> &threadAllocat
 
                     // Compute costs for splitting after each bucket
                     constexpr int nSplits = nBuckets - 1;
-                    float costs[nSplits] = {};
+                    Float costs[nSplits] = {};
                     // Partially initialize _costs_ using a forward scan over splits
                     int countBelow = 0;
                     Bounds3f boundBelow;
@@ -300,7 +300,7 @@ BVHBuildNode *BVHAggregate::buildRecursive(ThreadLocal<Allocator> &threadAllocat
                         costs[i] += countBelow * boundBelow.SurfaceArea();
                     }
 
-                    // Finish initializing _costs_ using a backwards scan over splits
+                    // Finish initializing _costs_ using a backward scan over splits
                     int countAbove = 0;
                     Bounds3f boundAbove;
                     for (int i = nSplits; i >= 1; --i) {

src/pbrt/cpu/integrators.h
@@ -62,7 +62,7 @@ class Integrator {
     std::vector<Light> infiniteLights;
 
   protected:
-    // Integrator Private Methods
+    // Integrator Protected Methods
     Integrator(Primitive aggregate, std::vector<Light> lights)
         : aggregate(aggregate), lights(lights) {
         // Integrator constructor implementation

src/pbrt/lights.cpp
@@ -133,11 +133,11 @@ Float LightBounds::Importance(Point3f p, Normal3f n) const {
         cosTheta_w = std::abs(cosTheta_w);
     Float sinTheta_w = SafeSqrt(1 - Sqr(cosTheta_w));
 
-    // Compute $\cos \theta_\roman{b}$ for reference point
+    // Compute $\cos\,\theta_\roman{\+b}$ for reference point
     Float cosTheta_b = BoundSubtendedDirections(bounds, p).cosTheta;
     Float sinTheta_b = SafeSqrt(1 - Sqr(cosTheta_b));
 
-    // Compute $\cos \theta'$ and test against $\cos \theta_\roman{e}$
+    // Compute $\cos\,\theta'$ and test against $\cos\,\theta_\roman{e}$
     Float sinTheta_o = SafeSqrt(1 - Sqr(cosTheta_o));
     Float cosTheta_x = cosSubClamped(sinTheta_w, cosTheta_w, sinTheta_o, cosTheta_o);
     Float sinTheta_x = sinSubClamped(sinTheta_w, cosTheta_w, sinTheta_o, cosTheta_o);
@@ -148,7 +148,7 @@ Float LightBounds::Importance(Point3f p, Normal3f n) const {
     // Return final importance at reference point
     Float importance = phi * cosThetap / d2;
     DCHECK_GE(importance, -1e-3);
-    // Account for $\cos \theta_\roman{i}$ in importance at surfaces
+    // Account for $\cos\theta_\roman{i}$ in importance at surfaces
     if (n != Normal3f(0, 0, 0)) {
         Float cosTheta_i = AbsDot(wi, n);
         Float sinTheta_i = SafeSqrt(1 - Sqr(cosTheta_i));
@@ -1260,7 +1260,7 @@ Float PortalImageInfiniteLight::PDF_Li(LightSampleContext ctx, Vector3f w,
     // Return PDF for sampling $(u,v)$ from reference point
     pstd::optional<Bounds2f> b = ImageBounds(ctx.p());
     if (!b)
-        return {};
+        return 0;
     Float pdf = distribution.PDF(*uv, *b);
     return pdf / duv_dw;
 }

commit db03ad9b70c0f1ebc88d3fb199c9f7331b8bd1d1

Fix bug in PBRT_FLOAT_AS_DOUBLE build introduced in 20673d2d8ac6b9

src/pbrt/cpu/integrators.cpp
@@ -146,7 +146,7 @@ void ImageTileIntegrator::Render() {
         Film film = camera.GetFilm();
         DisplayDynamic(film.GetFilename(), Point2i(pixelBounds.Diagonal()),
                        {"R", "G", "B"},
-                       [&](Bounds2i b, pstd::span<pstd::span<Float>> displayValue) {
+                       [&](Bounds2i b, pstd::span<pstd::span<float>> displayValue) {
                            int index = 0;
                            for (Point2i p : b) {
                                RGB rgb = film.GetPixelRGB(pixelBounds.pMin + p,