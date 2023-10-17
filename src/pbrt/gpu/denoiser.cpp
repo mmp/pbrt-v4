44a61fad814de0a95a4d582f0b0db20e89394360
Mike <mango@live.cn>
2023-10-17 13:17:24 +0800

    compatibility with optix 8

--- src/pbrt/gpu/denoiser.cpp
+++ src/pbrt/gpu/denoiser.cpp
@@ -40,7 +40,9 @@ Denoiser::Denoiser(Vector2i resolution, bool haveAlbedoAndNormal)
     OPTIX_CHECK(optixDeviceContextCreate(cudaContext, 0, &optixContext));
 
     OptixDenoiserOptions options = {};
-#if (OPTIX_VERSION >= 70300)
+#if (OPTIX_VERSION >= 80000)
+    options.denoiseAlpha = OPTIX_DENOISER_ALPHA_MODE_COPY;
+#elif (OPTIX_VERSION >= 70300)
     if (haveAlbedoAndNormal)
         options.guideAlbedo = options.guideNormal = 1;
 
@@ -101,7 +103,9 @@ void Denoiser::Denoise(RGB *rgb, Normal3f *n, RGB *albedo, RGB *result) {
         CUdeviceptr(scratchBuffer), memorySizes.withoutOverlapScratchSizeInBytes));
 
     OptixDenoiserParams params = {};
-#if (OPTIX_VERSION >= 70500)
+#if (OPTIX_VERSION >= 80000)
+    // denoiseAlpha is moved to OptixDenoiserOptions in OptiX 8.0
+#elif (OPTIX_VERSION >= 70500)
     params.denoiseAlpha = OPTIX_DENOISER_ALPHA_MODE_COPY;
 #else
     params.denoiseAlpha = 0;
